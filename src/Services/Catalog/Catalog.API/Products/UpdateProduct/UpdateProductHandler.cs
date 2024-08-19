2ae1a26e8baa3bdb22e9f673a6c34d82a495da22
AlShr <shramko.zp@gmail.com>
2024-08-19 18:09:49 +0300

    Crud Command Validators with Fluent Validation Pipeline behavior

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -4,6 +4,18 @@
 
   public record UpdateProductResult(bool IsSuccess);
 
+  public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+  {
+    public UpdateProductCommandValidator()
+    {
+      RuleFor(x => x.Id).NotEmpty().WithMessage("Product Id is required");
+      RuleFor(x => x.Name)
+        .NotEmpty().WithMessage("Name is required")
+        .Length(2,150).WithMessage("Name must be between 2 and 150 characters");
+      RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater then 0");
+    }
+  }
+
   internal class UpdateProductCommandHandler(IDocumentSession session) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
   {
     public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
