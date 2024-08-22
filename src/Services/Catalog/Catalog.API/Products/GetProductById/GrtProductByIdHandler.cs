cb7f040e857075d7dd5bf87d408394f0a6cf1f96
AlShr <shramko.zp@gmail.com>
2024-08-22 14:29:25 +0300

    Logging Behavior in MediatR Pipeline Crosscuting concerns

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GrtProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GrtProductByIdHandler.cs
@@ -4,12 +4,10 @@
 
   public record GetProductByIdResult(Product Product);
 
-  internal class GrtProductByIdQueryHandler(IDocumentSession session, ILogger<GrtProductByIdQueryHandler> logger): IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+  internal class GrtProductByIdQueryHandler(IDocumentSession session): IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
   {
     public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
     {
-      logger.LogInformation($"GrtProductByIdQueryHandler.Handler called with {query}", query);
-
       var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
       if (product is null)
       {
